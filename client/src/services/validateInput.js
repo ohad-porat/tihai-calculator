f0ed7c9527ec5060d0ea374181364d44eea13ab1
Ohad Porat <ohadp23@gmail.com>
2021-04-14 10:05:15 -0400

    added error validation for time signature

--- client/src/services/validateInput.js
+++ client/src/services/validateInput.js
@@ -1,21 +1,48 @@
 const validateInput = (phrase, timeSignature, subdivision) => {
   let submitErrors = {}
+  const timeSignatureArray = timeSignature.split("/")
+  const timeSignatureDown = timeSignatureArray[1]
+
   if (phrase.trim() === "") {
     submitErrors = {
       ...submitErrors,
       ["phrase"]: "cannot be empty",
     }
   }
+
+  if (subdivision.trim() === "") {
+    submitErrors = {
+      ...submitErrors,
+      ["subdivision"]: "cannot be empty",
+    }
+  }
+
   if (timeSignature.trim() === "") {
     submitErrors = {
       ...submitErrors,
       ["time signature"]: "cannot be empty",
     }
   }
-  if (subdivision.trim() === "") {
+
+  if (!timeSignature.includes("/") && timeSignature.trim() !== "") {
     submitErrors = {
       ...submitErrors,
-      ["subdivision"]: "cannot be empty",
+      ["time signature"]: "top and bottom number needs to be divided by a '/'",
+    }
+  }
+
+  if (
+    timeSignatureDown !== "2" &&
+    timeSignatureDown !== "4" &&
+    timeSignatureDown !== "8" &&
+    timeSignatureDown !== "16" &&
+    timeSignatureDown !== "32" &&
+    timeSignature.trim() !== "" &&
+    timeSignature.includes("/")
+  ) {
+    submitErrors = {
+      ...submitErrors,
+      ["time signature"]: "bottom number can only be 2, 4, 8, 16 or 32",
     }
   }
 
