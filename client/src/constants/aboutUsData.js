f9d7cbb51ed5189cf6ebe2cab984ef81ce32c328
Ohad Porat <ohadp23@gmail.com>
2021-04-15 11:32:42 -0400

    added about us page

---
+++ client/src/constants/aboutUsData.js
@@ -0,0 +1,18 @@
+const aboutUsData = [
+  {
+    name: "Yogev Gabay",
+    description:
+      "Yogev is a drummer, educator and the founder of the Time Consuming YouTube channel.",
+    youtubeUrl: "https://www.youtube.com/channel/UCZgsMhQWvCdudLK8Yn1aTfQ",
+    patreonUrl: "https://www.patreon.com/yogevgabay",
+  },
+  {
+    name: "Ohad Porat",
+    description:
+      "some text some text some text some text some text some text some text some text",
+    linkedinUrl: "https://www.linkedin.com/in/ohad-porat/",
+    githubUrl: "https://github.com/ohad-porat",
+  },
+]
+
+export default aboutUsData
