6c5d014481a808e44866adbbd9277878d2466548
Ohad Porat <ohadp23@gmail.com>
2021-05-09 13:15:09 -0400

    added activeClassName to navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import { hot } from "react-hot-loader/root"
 
-import getCurrentUser from "../services/getCurrentUser"
 import "../assets/scss/main.scss"
 import TopBar from "./layout/TopBar"
 import LandingPage from "./layout/tihaiCalculator/TihaiLandingPage"
@@ -10,20 +9,10 @@ import AboutUs from "./layout/AboutUs"
 import WhatIsTihai from "./layout/WhatIsTihai"
 import MeshuggahMainPage from "./layout/meshuggahCalculator/MeshuggahMainPage"
 
-const App = (props) => {
-  const [currentUser, setCurrentUser] = useState(undefined)
-  useEffect(() => {
-    getCurrentUser()
-      .then((user) => {
-        setCurrentUser(user)
-      })
-      .catch(() => {
-        setCurrentUser(null)
-      })
-  }, [])
+const App = () => {
   return (
     <Router>
-      <TopBar user={currentUser} />
+      <TopBar />
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/about-us" component={AboutUs} />
